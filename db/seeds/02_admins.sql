440ead7b1e2a2a095024c1daf68d816d8edecbe4
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-02-20 14:40:24 -0500

    Add seeds to the tables.

---
+++ db/seeds/02_admins.sql
@@ -0,0 +1,7 @@
+-- admins table seeds
+INSERT INTO admins (firstName, lastName, address, phone, email, password)
+VALUES ('labber', 'labber','Lighthouse Labs','123-456-7890','l@l.com', 'labber'),
+('Fatima', 'Siddique','Lighthouse Labs','647.477.7850','fatimasidq@gmail.com', '123'),
+('Hamza', 'Sohaib', 'Lighthouse Labs','226.506.3565',' hamzasohaib1998@gmail.com','123'),
+('Stellaris', 'Cano', 'Lighthouse Labs','647-704-8000','goodwisheseveryone@gmail.com','123'),
+('Test', 'Employee', 'Address1','111-222-3333','t@t.com','123');
