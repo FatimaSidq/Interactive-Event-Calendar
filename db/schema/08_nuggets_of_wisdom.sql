ef4824276ceecb61950d3c4e634ebd7ac8dbd938
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-02-19 20:17:43 -0500

    Modify table definition.

--- db/schema/08_nuggets_of_wisdom.sql
+++ db/schema/08_nuggets_of_wisdom.sql
@@ -4,6 +4,8 @@ CREATE TABLE nuggets_of_wisdom (
   id SERIAL PRIMARY KEY NOT NULL,
   nugget_of_wisdom VARCHAR(255),
   quote_by VARCHAR(50) DEFAULT NULL,
+  -- category VARCHAR(25) NOT NULL,
+  topic VARCHAR(50) NOT NULL,
   more_info TEXT NULL,
   banner_id INTEGER NOT NULL REFERENCES banners(id) ON DELETE CASCADE, 
   added_on TIMESTAMP NOT NULL DEFAULT Now(),
